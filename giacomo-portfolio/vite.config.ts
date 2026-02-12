31114cbca5ecd77580323f1fb68b1e9a81a85afe
GiacomoGe <giacomo.gerbotto@gmail.com>
2026-02-12 01:46:13 +0000

    Aggiunta config base per deploy

---
+++ giacomo-portfolio/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // IMPORTANTE: Sostituisci 'NOME-REPO' con il nome della tua repository!
+  // Esempio: se la repo è 'giacomo-portfolio', scrivi '/giacomo-portfolio/'
+  base: '/giacomo-portfolio/', 
+})
